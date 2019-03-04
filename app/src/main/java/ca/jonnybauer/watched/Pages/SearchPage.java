2d2d9017f2d104db197af9cf43d410da8e300cf9
Jonathon Bauer <jonathon.bauer01@stclairconnect.ca>
2019-03-04 13:24:09 -0500

    #46 Completed the Search Page ListView Adapter

--- app/src/main/java/ca/jonnybauer/watched/Pages/SearchPage.java
+++ app/src/main/java/ca/jonnybauer/watched/Pages/SearchPage.java
@@ -7,7 +7,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
+import java.util.ArrayList;
+
+import ca.jonnybauer.watched.Models.Movie;
 import ca.jonnybauer.watched.R;
 
 /**
@@ -37,6 +42,48 @@ public class SearchPage extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search_page, container, false);
     }
+
+    /**
+     * This adapter is going to be used to load the ListView on the search page with all the results.
+     * It is an extension of the ArrayAdapter class
+     *
+     * @author Jonathon Bauer
+     * @version 1.0
+     * @see android.widget.ArrayAdapter
+     */
+    public class SearchResultAdapter extends ArrayAdapter<Movie> {
+
+        // Constructor
+        public SearchResultAdapter(Context context, ArrayList<Movie> results) {
+            super(context, 0, results);
+        }
+
+        // getView method
+        public View getView(int position, View view, ViewGroup parent) {
+
+            // Check if the view has been inflated, and if it hasn't inflate it
+            if(view == null) {
+                view = LayoutInflater.from(getContext()).inflate(R.layout.search_result, parent, false);
+            }
+
+            // Get the layout elements
+            TextView title = view.findViewById(R.id.resultViewTitle);
+            TextView rating = view.findViewById(R.id.resultViewRating);
+
+            // Set the layout elements to the item in the current position
+            Movie result = getItem(position);
+            title.setText(result.getTitle());
+            rating.setText(result.getRating());
+
+            return view;
+        }
+
+
+    }
+
+
+
+
 
     @Override
     public void onAttach(Context context) {
