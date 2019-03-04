2c8291de7c45cf2e62323552e27245e1f1f79121
Jonathon Bauer <jonathon.bauer01@stclairconnect.ca>
2019-03-04 12:17:56 -0500

    #17 #20 Completed basic Search Page Layout and Search Card View layout

---
+++ app/src/main/java/ca/jonnybauer/watched/Pages/SearchPage.java
@@ -0,0 +1,62 @@
+package ca.jonnybauer.watched.Pages;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ca.jonnybauer.watched.R;
+
+/**
+ *
+ * This Fragment is used to display and manipulate the search page.
+ *
+ * @author Jonathon Bauer
+ * @version 1.0
+ *
+ */
+public class SearchPage extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+
+    public SearchPage() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search_page, container, false);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
